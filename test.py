19d63f0e5c40035d7a777cbb597307a429219c8c
Jack <cestcest@163.com>
2016-12-08 15:17:26 +0800

    Get HQ and Base data of all stock.

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 # encoding: UTF-8
 
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta
 import pandas as pd
 import tushare as ts
-import timeit as ti
-import easytrader
+import easytrader as et
 
 '''
 code,代码
@@ -49,96 +48,128 @@ holders,股东人数
 •nmc:流通市值
 '''
 
+
+def rank_stock(stockcode_list):    # 股票评分
+    dst_stocks = {}
+    for stock in stockcode_list:
+        his = ts.get_k_data(stock, ktype='D')
+        his15 = his.tail(15)
+        his130 = his.tail(130)
+        low_price_130 = his130.low.min()
+        high_price_130 = his130.high.max()
+
+        avg_15 = his15.close.astype('float64').mean()
+        cur_price = his15.iloc[-1]['close']
+
+        score = (cur_price - low_price_130) + (cur_price - high_price_130) + (cur_price - avg_15)
+        dst_stocks[stock] = score
+
+    df_stock = pd.DataFrame.from_dict(dst_stocks, orient='index')
+    df_stock.columns = ['score']
+    df_stock = df_stock.sort_values(by='score', ascending=True)
+
+    return df_stock.index
+
+
+def get_stock_data(filename):
+    # 基本面数据
+    basic = ts.get_stock_basics()
+    # 行情和市值数据
+    hq = ts.get_today_all()
+
+    # 当前股价,如果停牌则设置当前价格为上一个交易日股价
+    # hq['trade'] = hq.apply(lambda x: x.settlement if x.trade == 0 else x.trade, axis=1)
+
+    # 分别选取流通股本,总股本,每股公积金,每股收益
+    basedata = basic[['esp', 'timeToMarket', 'profit']]
+
+    # 选取股票代码,名称,当前价格,总市值,流通市值
+    hqdata = hq[['code', 'name', 'trade', 'mktcap', 'turnoverratio', 'changepercent']]
+    hqdata = hqdata[hqdata['trade'] > 0]
+    hqdata = hqdata[hqdata.code.map(lambda x: not x.startswith('300'))]
+    hqdata = hqdata[hqdata.name.map(lambda x: 'ST' not in x)]
+    hqdata = hqdata[hqdata.name.map(lambda x: 'N' not in x)]
+    hqdata = hqdata[hqdata.name.map(lambda x: u'退市' not in x)]
+
+    # 设置行情数据code为index列
+    hqdata = hqdata.set_index('code')
+
+    # 合并两个数据表
+    stocksdata = hqdata.merge(basedata, left_index=True, right_index=True)
+
+    # 将总市值和流通市值换成亿元单位
+    stocksdata['mktcap'] /= 10000
+    # 保存到excel文件
+    stocksdata.to_excel(filename)
+
+
+def select_stock(filename):
+    today = date.today()
+    week4 = today - timedelta(days=60)
+    valid_day = int(week4.strftime('%Y%m%d'))
+    df = pd.read_excel(filename, encoding='gbk', converters={'code': str, 'timeToMarket': int})
+    df = df[df['esp'] > 0]
+
+
+    df = df[df['timeToMarket'] < valid_day]
+    df = df.sort_values(by='mktcap', ascending=True)
+    stocks = df[0:25]
+    stock_list = list(stocks['code'])
+
+    return rank_stock(stock_list)
+
+
+def stop_loss_index(index):
+    his = ts.get_k_data(index, index=True, ktype='D')
+    his160 = his.tail(160)
+    print(his160)
+    today_max = his160.high.max()
+    today_min = his160.low.min() * 2.2
+    close1 = his160.iloc[-1]['close']
+    close4 = his160.iloc[-4]['close']
+    close100 = his160.iloc[-100]['close']
+
+    return today_max > today_min and close4 > close1 > close100
+
+
+def get_index_four_week_chg(index):
+    df_index = ts.get_k_data(index, index=True, ktype='W')
+    index0 = df_index.iloc[-1]['close']
+    index4 = df_index.iloc[-5]['close']
+
+    if index4 != 0:
+        return (index0/index4 - 1) * 100
+    else:
+        return 0
+
 index2 = '000016'  # 上证50指数，表示二，大盘股
 index8 = '399333'  # 中小板R指数，表示八，小盘股
 
-# df = ts.get_realtime_quotes('162411')
 today = date.today()
-week4 = today - timedelta(days=90)
+tomorrow = today + timedelta(days=1)
+week4 = today - timedelta(days=60)
 yesterday = today - timedelta(days=1)
 lastday = today - timedelta(days=600)
-print(yesterday, lastday)
-filename = 'stockdata%s.xls' % today
+print(tomorrow,today, yesterday, lastday)
+filepath = 'stockdata%s.xls' % today
+
+# df = ts.get_realtime_quotes('162411')
+
 # df = ts.get_k_data('000001', index=True, ktype='D', start=week4.strftime('%Y-%m-%d'), end=today.strftime('%Y-%m-%d'))
 # print(df)
 # stock_price = df['ask'][0]
 
-# df = ts.get_k_data('000001', index=True, ktype='W')
-# print(df, df.iloc[-1]['close'],df.iloc[-4]['close'])
-
-# df2 = ts.get_k_data(index2, index=True, ktype='D')
-# print(df2, df2.iloc[-1]['close'], df2.iloc[-20]['close'])
+# print(stop_loss_index('000001'))
+print(get_index_four_week_chg(index2))
+print(get_index_four_week_chg(index8))
+# df2 = ts.get_k_data(index2, index=True, ktype='W')
+# print(df2, df2.iloc[-1]['close'], df2.iloc[-4]['close'])
 
 # df8 = ts.get_k_data(index8, index=True, ktype='D')
 # print(df8, df8.iloc[-1]['close'], df8.iloc[-20]['close'])
+# get_stock_data(filepath)
 
-'''
-# 基本面数据
-basic = ts.get_stock_basics()
-# 行情和市值数据
-hq = ts.get_today_all()
-
-# 当前股价,如果停牌则设置当前价格为上一个交易日股价
-# hq['trade'] = hq.apply(lambda x: x.settlement if x.trade == 0 else x.trade, axis=1)
-
-# 分别选取流通股本,总股本,每股公积金,每股收益
-basedata = basic[['esp', 'timeToMarket', 'profit']]
-
-# 选取股票代码,名称,当前价格,总市值,流通市值
-df = hq[['code', 'name', 'trade', 'mktcap']]
-df = df[df['trade'] > 0]
-df = df[df.code.map(lambda x: not x.startswith('300'))]
-df = df[df.name.map(lambda x: 'ST' not in x)]
-df = df[df.name.map(lambda x: 'N' not in x)]
-df = df[df.name.map(lambda x: u'退市' not in x)]
-
-hqdata = df
-# 设置行情数据code为index列
-hqdata = hqdata.set_index('code')
-
-# 合并两个数据表
-stocksdata = hqdata.merge(basedata, left_index=True, right_index=True)
-
-# 将总市值和流通市值换成亿元单位
-stocksdata['mktcap'] /= 10000
-
-
-stocksdata.to_excel(filename)
-'''
-
-df = pd.read_excel(filename, encoding='gbk', converters={'code': str, 'timeToMarket': int})
-
-# df['timeToMarket'] = str.format(df['timeToMarket'],'%Y%M%D')
-df = df[df['esp'] > 0]
-vaildday = int(week4.strftime('%Y%m%d'))
-df = df[df['timeToMarket'] < vaildday]
-df = df.sort_values(by='mktcap', ascending=True)
-# stocks = df.set_index('mktcap')
-stocks = df[0:25]
-stock_list = list(stocks['code'])
-print(stock_list)
-
-dst_stocks = {}
-for stock in stock_list:
-    # his = ts.get_k_data(stock, ktype='D', start='2014-12-06', end='2016-12-06')
-    his = ts.get_k_data(stock, ktype='D')
-    his15 = his.tail(15)
-    his130 = his.tail(130)
-    low_price_130 = his130.low.min()
-    high_price_130 = his130.high.max()
-
-    avg_15 = his15.close.astype('float64').mean()
-    # print(avg_15)
-    cur_price = his15.iloc[-1]['close']
-
-    score = (cur_price - low_price_130) + (cur_price - high_price_130) + (cur_price - avg_15)
-    dst_stocks[stock] = score
-
-df = pd.DataFrame.from_dict(dst_stocks, orient='index')
-df.columns = ['score']
-df = df.sort_values(by='score', ascending=True)
-stocks = df.index
-print(df, stocks)
+# print(select_stock(filepath))
 
 # print(his.low.min(), his.high.max(), his.iloc[-1]['close'])
 
@@ -150,24 +181,4 @@ print(df, stocks)
 # ret = yjb.sell('162411', price=stock_price, amount=100)
 # print('ret', ret)
 
-# 股票评分
-def rank_stocks(data, stock_list):
-    dst_stocks = {}
-    for stock in stock_list:
-        df = ts.get_k_data(stock, ktype='D', start=lasyday130.strftime('%Y-%m-%d'),
-                           end=today.strftime('%Y-%m-%d'))
-        h = attribute_history(stock, 130, unit='1d', fields=('close', 'high', 'low'), skip_paused=True)
-        low_price_130 = h.low.min()
-        high_price_130 = h.high.max()
 
-        avg_15 = data[stock].mavg(15, field='close')
-        cur_price = data[stock].close
-
-        score = (cur_price - low_price_130) + (cur_price - high_price_130) + (cur_price - avg_15)
-        dst_stocks[stock] = score
-
-    df = pd.DataFrame(dst_stocks.values(), index=dst_stocks.keys())
-    df.columns = ['score']
-    df = df.sort(columns='score', ascending=True)
-
-    return df.index
